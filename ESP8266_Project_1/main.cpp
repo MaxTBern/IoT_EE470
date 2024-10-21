f248eaa4bf9a96ee44f186a3537f5240e3d4e8f9
Max Bernstein <159392123+MaxTBern@users.noreply.github.com>
2024-10-20 23:40:21 -0700

    Create main.cpp

---
+++ ESP8266_Project_1/main.cpp
@@ -0,0 +1,60 @@
+/*I'M NOT WRITING A HEADER YOU CAN'T MAKE ME
+
+
+*/
+
+#include <Arduino.h>
+#include <ArduinoJson.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <stdlib.h>
+#include "dhtnew.h"
+#include "sensorData.h"
+
+const char* ssid = "TheShire";
+const char* password = "Potaytoe";
+
+DHTNEW dht(10);
+WiFiClientSecure client;
+HTTPClient https;
+sensorData sensor(16, 1);
+String myTime;
+int temperature;
+int humidity;
+String test;
+int timezone;
+
+
+void setup() {
+  Serial.begin(9600);
+  client.setInsecure();
+  timezone = sensor.selectTimeZone();
+  sensor.defineTimeZone(timezone);
+  sensor.getWiFi(ssid, password);
+  dht.setHumOffset(-800);
+  dht.setTempOffset(-595);
+}
+
+
+
+void loop() {
+  sensor.checkSwitch(sensor);
+
+  myTime = sensor.readTime(https, client);  
+
+  temperature = sensor.readSensorTemp(dht);
+  
+  humidity = sensor.readSensorHum(dht);
+
+  if (!(sensor.checkError(temperature, humidity))) {
+    Serial.println("Values are a go!");
+    while (Serial.available()) {
+        Serial.read();
+    }while (Serial.available() == 0) {
+        delay(100);
+    }
+    sensor.transmit(myTime, temperature, humidity, client, https);
+  }
+}
+
+
