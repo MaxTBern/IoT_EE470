c0d5a74054fbdc6ed7600b26a4d9f9a124acad97
Max Bernstein <159392123+MaxTBern@users.noreply.github.com>
2024-10-30 00:59:39 -0700

    Create api.php

---
+++ API_Assignment/HTML & PHP Code/api.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+       
+    <title>Dashboard</title>
+        <head>
+            <h1>View the Google Sheet Values!</h1>
+        </head>
+        <iframe width="600" height="371" seamless frameborder="0" scrolling="no" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vR-j8EWDltoJIq_CVfLq9P5D6Y6PqB0Zannj3qzbfuMeonnYTC3Obh-qIPo4Zq9vh1pSdcR1-Q8VG3S/pubchart?oid=882385983&amp;format=interactive"></iframe><br>
+        <iframe width="708" height="190" seamless frameborder="0" scrolling="no" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vR-j8EWDltoJIq_CVfLq9P5D6Y6PqB0Zannj3qzbfuMeonnYTC3Obh-qIPo4Zq9vh1pSdcR1-Q8VG3S/pubchart?oid=1853620631&amp;format=interactive"></iframe>
+        <form method="POST" action="https://script.google.com/macros/s/AKfycbwSpNrQI7ehOoATvO-Xm_uiTJyuJBBjpZr9X_X0cue_NTOcx4iAUFok7ZA4sw2Eqc5psQ/exec">
+            <label>New Sensor Value (taken @ current time): </label>
+            <input type="text" name="sensor" required>
+            <input type="submit">
+        </form>
+</html>
